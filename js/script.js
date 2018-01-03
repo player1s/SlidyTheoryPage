6cb3c61549bbc220c79f256e959b72a1460d3f1a
Sámán <31960469+player1s@users.noreply.github.com>
2018-01-03 14:18:22 +0100

    Slidy structure on mainpage

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-var height = window.height;
-var width = window.width;
+height = $(window).height();
+width = $(window).width();
 
 $(document).bind('mousemove',function(e){ 
-    $("#services").css("margin-left", e.pageX);
-    $("#portfolio").css("margin-left", 100+e.pageX);
-    $("#contact").css("margin-left", e.pageX);
-    $("#about").css("margin-left", e.pageX);
+    $("#services").css("margin-left",  0 + 100 -(e.pageX/10));
+    $("#portfolio").css("margin-left", width + 100 -(e.pageX/10));
+    $("#contact").css("margin-left",  0 + 100 -(e.pageX/10));
+    $("#about").css("margin-left",  width + 100 -(e.pageX/10));
 });
