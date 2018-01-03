5459a0fb1b200ef1d707296a3bec3a2fd2bc8f0e
Sámán <31960469+player1s@users.noreply.github.com>
2018-01-03 15:24:49 +0100

    main page slide 2. axis

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 height = $(window).height();
 width = $(window).width();
-
-$(document).bind('mousemove',function(e){ 
-    $("#services").css("margin-left",  0 + 100 -(e.pageX/10));
-    $("#portfolio").css("margin-left", width + 100 -(e.pageX/10));
-    $("#contact").css("margin-left",  0 + 100 -(e.pageX/10));
-    $("#about").css("margin-left",  width + 100 -(e.pageX/10));
+$(document).bind('mousemove', function (e) {
+    $("#services").css("margin-left", 0 + width / 10 - (e.pageX / 7));
+    $("#services").css("margin-top", height - (e.pageY / 3));
+    $("#portfolio").css("margin-left", width + width / 200 - (e.pageX / 7));
+    $("#portfolio").css("margin-top", height - (e.pageY / 3));
+    $("#contact").css("margin-left", 0 + width / 10 - (e.pageX / 7));
+    $("#contact").css("margin-top", 0 + height / 5 - (e.pageY / 3));
+    $("#about").css("margin-left", width + width / 200 - (e.pageX / 7));
+    $("#about").css("margin-top", 0 + height / 5 - (e.pageY / 3));
 });
