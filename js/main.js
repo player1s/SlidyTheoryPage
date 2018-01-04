8e5f54e9c52dadef6a37ad45ed69727a98bace92
Sámán <31960469+player1s@users.noreply.github.com>
2018-01-04 14:28:28 +0100

    centering the title

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var height = $(window).height();
 var width = $(window).width();
 $(document).bind('mousemove', function (e) {
     $("#servicesLink").css("margin-left", 0 + width / 10 - (e.pageX / 7));
-    $("#servicesLink").css("margin-top", height - (e.pageY / 3));
+    $("#servicesLink").css("margin-top", height - height / 10 - (e.pageY / 3));
     $("#portfolioLink").css("margin-left", width + width / 200 - (e.pageX / 7));
     $("#portfolioLink").css("margin-top", height - (e.pageY / 3));
     $("#contactLink").css("margin-left", 0 + width / 10 - (e.pageX / 7));
@@ -18,3 +18,14 @@ $(document).bind('mousemove', function (e) {
     $("#riseTheoryTitle").css("margin-left", 0 + width / 2 + ((e.pageX-width/2) / 3));
 });
 
+$("#riseTheoryTitle").css("margin-top", 0 + height / 2 );
+    $("#riseTheoryTitle").css("margin-left", 0 + width / 2 );
+
+ $("#servicesLink").css("margin-left", 0 + width / 10);
+    $("#servicesLink").css("margin-top", height - height / 10 );
+    $("#portfolioLink").css("margin-left", width - width / 10 );
+    $("#portfolioLink").css("margin-top", height - height / 10 );
+    $("#contactLink").css("margin-left", 0 + width / 10 );
+    $("#contactLink").css("margin-top", 0 + height / 10 );
+    $("#aboutLink").css("margin-left", width - width / 10 );
+    $("#aboutLink").css("margin-top", 0 + height / 10 );
