c9c88ae786f56a1a2b1935020390c21432740386
mvilares <martivilares@gmail.com>
2018-01-04 14:08:50 +0100

    Added transition animations for al the pages

--- js/about.js
+++ js/about.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
     $("#aboutLink").click(function(){
         moveToAboutFromMain();
-        moveMainAway();
+        moveMainAwayFromAbout();
     });
-});
-
-$("#aboutWrapper").css({
-    "position": "absolute",
-    "left": width,
-    "top": -height,
-    "width": width,
-    "height": height,
-    "border": "solid 5px blue"
+    $("#servicesLink").click(function(){
+        moveToServicesFromMain();
+        moveMainAwayFromServices();
+    });
+    $("#portfolioLink").click(function(){
+        moveToPortfolioFromMain();
+        moveMainAwayFromPortfolio();
+    });
+    $("#contactLink").click(function(){
+        moveToContactFromMain();
+        moveMainAwayFromContact();
+    });
 });
 
 $("#mainWrapper").css({
@@ -23,6 +26,15 @@ $("#mainWrapper").css({
     "border": "solid 5px red"
 });
 
+$("#aboutWrapper").css({
+    "position": "absolute",
+    "left": width,
+    "top": -height,
+    "width": width,
+    "height": height,
+    "border": "solid 5px blue"
+});
+
 function moveToAboutFromMain() {
     $("#aboutWrapper").animate({
             "left": "0px",
@@ -30,9 +42,78 @@ function moveToAboutFromMain() {
         }, 3000);
 }
 
-function moveMainAway() {
+function moveMainAwayFromAbout() {
     $("#mainWrapper").animate({
             "left": -width,
+            "top": height
+        }, 3000);
+}
+
+$("#servicesWrapper").css({
+    "position": "absolute",
+    "left": -width,
+    "top": height,
+    "width": width,
+    "height": height,
+    "border": "solid 5px black"
+});
+
+function moveToServicesFromMain() {
+    $("#servicesWrapper").animate({
+            "left": "0px",
+            "top": "0px"
+        }, 3000);
+}
+
+function moveMainAwayFromServices() {
+    $("#mainWrapper").animate({
+            "left": width,
+            "top": -height
+        }, 3000);
+}
+
+$("#portfolioWrapper").css({
+    "position": "absolute",
+    "left": width,
+    "top": height,
+    "width": width,
+    "height": height,
+    "border": "solid 5px green"
+});
+
+function moveToPortfolioFromMain() {
+    $("#portfolioWrapper").animate({
+            "left": "0px",
+            "top": "0px"
+        }, 3000);
+}
+
+function moveMainAwayFromPortfolio() {
+    $("#mainWrapper").animate({
+            "left": -width,
+            "top": -height
+        }, 3000);
+}
+
+$("#contactWrapper").css({
+    "position": "absolute",
+    "left": -width,
+    "top": -height,
+    "width": width,
+    "height": height,
+    "border": "solid 5px pink"
+});
+
+function moveToContactFromMain() {
+    $("#contactWrapper").animate({
+            "left": "0px",
+            "top": "0px"
+        }, 3000);
+}
+
+function moveMainAwayFromContact() {
+    $("#mainWrapper").animate({
+            "left": width,
             "top": height
         }, 3000);
 }
