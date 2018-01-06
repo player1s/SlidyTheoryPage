c4a3588f3af3e5381edf1d6fbddc5e4af9909bf9
Sámán <31960469+player1s@users.noreply.github.com>
2018-01-06 20:28:20 +0100

    base structure html

--- js/main.js
+++ js/main.js
@@ -9,13 +9,22 @@ $(document).bind('mousemove', function (e) {
     $("#portfolioLink").css("margin-top", height - height / 10 - (e.pageY / 3));
     
     $("#contactLink").css("margin-left", 0 + width / 10 - (e.pageX / 7));
-    $("#contactLink").css("margin-top", 0 + height / 5 - (e.pageY / 3));
+    $("#contactLink").css("margin-top", 0 + height / 4 - (e.pageY / 3));
     
     $("#aboutLink").css("margin-left", width + width / 200 - (e.pageX / 7));
-    $("#aboutLink").css("margin-top", 0 + height / 5 - (e.pageY / 3));
+    $("#aboutLink").css("margin-top", 0 + height / 4 - (e.pageY / 3));
     
-    $("#riseTheoryTitle").css("margin-top", 0 + height / 2 + ((e.pageY-height/2) / 3));
+    $("#riseTheoryTitle").css("margin-top", 0 + height / 3 + ((e.pageY-height/2) / 3));
     $("#riseTheoryTitle").css("margin-left", 0 + width / 2 + ((e.pageX-width/2) / 3));
+    
+    $("#motto").css("margin-top", 0 + height / 1.4 );
+    $("#motto").css("margin-left", 0 + width / 2.5 + ((e.pageX-width/2) / 3));
+    
+    $("#motto2").css("margin-left", 0 + width / 10 - (e.pageX / 7));
+    $("#motto2").css("margin-top", 0 + height / 2);
+    
+    $("#logo").css("margin-left", width * 0.87 - (e.pageX / 7));
+    $("#logo").css("margin-top", 0 + height / 3);
 });
 /* STarting position */
 $("#riseTheoryTitle").css("margin-top", 0 + height / 2 );
